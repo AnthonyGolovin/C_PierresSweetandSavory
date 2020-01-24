9372b468069d2e9f8ac235259c503d00b9fa9e9d
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2020-01-24 11:20:49 -0800

    Add flavor cshtml pages and authentication

---
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetAndSavory.Models
+{
+  public class SweetAndSavoryContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+
+    public LibraryContext(DbContextOptions options) : base(options) { }
+  }
+}
